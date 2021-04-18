479ae30536c0865294b3be20629720dcba85f1bd
Emily Port <porte2@winthrop.edu>
2021-04-18 17:35:06 -0400

    folders were in the wrong places and unsaved files

--- HW4/client/main.js
+++ HW4/client/main.js
@@ -1,1 +1,22 @@
-console.log("log from main.js");
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+
+const renderPosts = function (passed_posts) {
+  console.log(passed_posts);
+  let formattedPosts = passed_posts.map(function(post){
+    return <p key={post._id}>{post.topic} have {post.votes} vote[s]</p>
+  });
+  return formattedPosts;
+};
+
+Meteor.startup(function() {
+  let title = "Fake Facebook";
+  // let jsx = (
+  //   <div>
+  //     <h1>{title}</h1>
+  //     {renderPosts(posts)}
+  //   </div>
+  // )
+
+});
--- HW4/server/main.js
+++ HW4/server/main.js
@@ -1,4 +1,12 @@
 console.log("log from server");
 import {Meteor} from 'meteor/meteor';
 
-import {UP_Collection_Access} from './../imports/api/user_posts.js'
+import {UP_Collection_Access} from './../imports/api/user_posts.js';
+
+Meteor.startup(function(){
+  UP_Collection_Access.insert({
+    topic: 'dog',
+    votes: 9,
+  });
+  console.log(UP_Collection_Access.find());
+});
