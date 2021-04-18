3cc5163e7d69096bb3d1cb3b1d15625cb80826c4
Emily Port <porte2@winthrop.edu>
2021-04-18 18:28:31 -0400

    upvote and downvote buttons added

--- HW4/client/main.js
+++ HW4/client/main.js
@@ -9,9 +9,15 @@ const renderPosts = function (passed_posts) {
   let formattedPosts = passed_posts.map(function(post){
     return (
     <p key={post._id}>{post.topic} have {post.votes} vote[s] {' '}
-    <button onClick={() => {
-      UP_Collection_Access.remove({_id: post._id});
-    }}>X</button>
+      <button onClick={() => {
+        UP_Collection_Access.update({_id: post._id}, {$inc: {votes: 1}});
+      }}>+1</button>
+      <button onClick={() => {
+        UP_Collection_Access.update({_id: post._id}, {$inc: {votes: -1}});
+      }}>-1</button>
+      <button onClick={() => {
+        UP_Collection_Access.remove({_id: post._id});
+      }}>X</button>
     </p>
     );
   });
