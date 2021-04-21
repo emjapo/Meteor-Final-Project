c74944186012aedfb0f5f6302f08b9be4cac7a39
Emily Port <porte2@winthrop.edu>
2021-04-21 13:27:03 -0400

    added renderpost component (finished vid 18c)

--- HW4/client/main.js
+++ HW4/client/main.js
@@ -4,23 +4,13 @@ import {Meteor} from 'meteor/meteor';
 import {UP_Collection_Access} from './../imports/api/user_posts.js';
 import TitleBar from './../imports/ui/TitleBar.js';
 import AddTopics from '../imports/ui/AddTopics.js';
+import RenderPost from '../imports/ui/RenderPost.js';
+
 
 
 const renderPosts = function (passed_posts) {
   let formattedPosts = passed_posts.map(function(post){
-    return (
-    <p key={post._id}>{post.topic} have {post.votes} vote[s] {' '}
-      <button onClick={() => {
-        UP_Collection_Access.update({_id: post._id}, {$inc: {votes: 1}});
-      }}>+1</button>
-      <button onClick={() => {
-        UP_Collection_Access.update({_id: post._id}, {$inc: {votes: -1}});
-      }}>-1</button>
-      <button onClick={() => {
-        UP_Collection_Access.remove({_id: post._id});
-      }}>X</button>
-    </p>
-    );
+    return <RenderPost key={post._id} post={post} />
   });
   return formattedPosts;
 };
---
+++ HW4/imports/ui/RenderPost.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {UP_Collection_Access} from './../api/user_posts.js';
+import PropTypes from 'prop-types';
+
+
+export default class RenderPost extends React.Component {
+    render() {
+        return (
+            <>
+                <p key={this.props.post._id}>{this.props.post.topic} have {this.props.post.votes} vote[s] {' '}
+                    <button onClick={() => {
+                        UP_Collection_Access.update({_id: this.props.post._id}, {$inc: {votes: 1}});
+                    }}>+1</button>
+                    <button onClick={() => {
+                        UP_Collection_Access.update({_id: this.props.post._id}, {$inc: {votes: -1}});
+                    }}>-1</button>
+                    <button onClick={() => {
+                        UP_Collection_Access.remove({_id: this.props.post._id});
+                    }}>X</button>
+                </p>
+            </>
+        )
+    }
+}
+
+RenderPost.propTypes = {
+    post: PropTypes.object.isRequired,
+};
