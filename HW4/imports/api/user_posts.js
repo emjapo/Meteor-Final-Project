e962dbec603b2effd426535cfac168c68bc9b20e
Emily Port <porte2@winthrop.edu>
2021-04-22 22:16:15 -0400

    Might make changes from vid 33b later

--- HW4/imports/api/user_posts.js
+++ HW4/imports/api/user_posts.js
@@ -2,5 +2,5 @@ import {Mongo} from 'meteor/mongo';
 
 
 export const UP_Collection_Access = new Mongo.Collection('user_posts_collection');
-
+// might add rank here for the extra practice I guess ugh
 export const Reply_Collection_Access = new Mongo.Collection('post_replies_collection');
